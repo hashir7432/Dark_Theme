f6dec0c13d522f598efaefa50c908eec65cdd9b6
hashir7432 <hashirmemon74@gmail.com>
2024-08-08 15:49:20 +0500

    My grocery app

---
+++ lib/login.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              // logo
+              Icon(
+                Icons.lock,
+                size: 100,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
